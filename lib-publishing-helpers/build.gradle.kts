0d9c851798cb6586a75dc84e20ba7285422b239e
Louis CAD <louis.cognault@gmail.com>
2021-07-11 20:18:35 +0200

    Use kotlin-test instead of plain JUnit4

--- lib-publishing-helpers/build.gradle.kts
+++ lib-publishing-helpers/build.gradle.kts
@@ -16,7 +16,7 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    testImplementation("junit", "junit", "4.12")
+    testImplementation(kotlin("test"))
 }
 
 configure<JavaPluginConvention> {
