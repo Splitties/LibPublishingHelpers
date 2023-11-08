aee83c96e2149e191c35fd76bbe0afa00a2106a6
Louis CAD <louis.cognault@gmail.com>
2023-11-08 14:47:18 +0100

    Suppress false positive unused warning

--- gradle-plugins/settings-include-dsl/src/main/kotlin/SettingsIncludeDslPlugin.kt
+++ gradle-plugins/settings-include-dsl/src/main/kotlin/SettingsIncludeDslPlugin.kt
@@ -3,6 +3,7 @@ package org.splitties.incubator.gradle
 import org.gradle.api.Plugin
 import org.gradle.api.initialization.Settings
 
+@Suppress("unused") // referenced in build.gradle.kts
 class SettingsIncludeDslPlugin : Plugin<Settings> {
     override fun apply(target: Settings) = Unit
 }
